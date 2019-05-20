6231189d42c02dd0a840ce8ad85a700880fb3a17
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-20 16:57:01 -0600

    Fix typo in OrderItem spec on belong_to :order

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe OrderItem, type: :model do
   describe 'relationships' do
     it { should belong_to :item}
-    it { should belong_to :user}
+    it { should belong_to :order}
   end
 
   describe 'validations' do
