9c7d738eef3d088c717fcb5379d424da09a87f3e
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-20 16:31:17 -0600

    Add validations to User model
    Co-authored-by: Vince Carollo <vincecarollo@gmail.com>
    Co-authored-by: Brennan Ayers <brennan.f.ayers@gmail.com>
    Co-authored-by: William Homer <billyhomeriii@gmail.com>
    Co-authored-by: Zach Leach <zachleach@techoverco.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates_presence_of :password_digest, :role, :active, :name, :address, :city, :state, :zip
+  validates :email, presence: true, uniqueness: true
 end
