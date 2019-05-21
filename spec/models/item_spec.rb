5fc4f883c9484942c8fea973237b196507c5b8cb
Vince Carollo <vincecarollo@gmail.com>
2019-05-20 20:43:45 -0600

    Add test for new item class method all_active

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,4 +13,31 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :image}
     it { should validate_presence_of :inventory}
   end
+
+  before :each do
+    merchant_1 = create(:user)
+    merchant_2 = create(:user)
+    @item_1 = create(:item, user: merchant_1)
+    @item_2 = create(:item, user: merchant_1)
+    @item_3 = create(:item, user: merchant_1)
+    @item_4 = create(:item, user: merchant_2)
+    @item_5 = create(:item, user: merchant_2, active: false)
+    @item_6 = create(:item, user: merchant_2, active: false)
+  end
+
+  describe 'class methods' do
+
+    it '.all_active' do
+      active = [@item_1, @item_2, @item_3, @item_4]
+      non_active = [@item_5, @item_6]
+
+      Item.all_active.each do |item|
+        expect(active.include?(item)).to be true
+        expect(non_active.include?(item)).to be false
+      end
+
+    end
+
+  end
+  
 end
