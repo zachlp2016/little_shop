a174fcc12fe7c70beb3e492788bb07c42f211ff9
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-21 15:51:16 -0600

    Add /register routes for User#new and User#create
    Co-authored-by: Vince Carollo <vincecarollo@gmail.com>
    Co-authored-by: Brennan Ayers <brennan.f.ayers@gmail.com>
    Co-authored-by: William Homer <billyhomeriii@gmail.com>
    Co-authored-by: Zach Leach <zachleach@techoverco.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  get '/profile/:id', to: 'users#show', as: :user
-
   resources :items, only: [:index, :show]
 
-  resources :users, only:[:new, :create, :edit]
+  resources :users, only:[:edit]
+
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
 
   get '/login', to: 'sessions#new', as: :login
   post '/login', to: 'sessions#create'
 
-  get '/dashboard', to: 'merchants#show'
+  get '/profile/:id', to: 'users#show', as: :user
 
+  get '/dashboard', to: 'merchants#show'
 end
