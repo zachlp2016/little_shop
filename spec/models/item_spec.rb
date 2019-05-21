cebd36c719d7265b454a36fba4461846b7d29b10
Vince Carollo <vincecarollo@gmail.com>
2019-05-20 19:40:34 -0600

    Change item model spec to check validation of boolean

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Item, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :name}
-    it { should validate_presence_of :active}
+    it { should validate_inclusion_of :active, in: [true, false]}
     it { should validate_presence_of :price}
     it { should validate_presence_of :description}
     it { should validate_presence_of :image}
