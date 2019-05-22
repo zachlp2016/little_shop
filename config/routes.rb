ad98a870ab0965ecbe916ef34b6a6b81e37939b8
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-21 23:18:11 -0600

    Add Patch profile/edit route to users#patch

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
+  patch 'profile/edit', to: 'users#patch'
 
   get '/dashboard', to: 'merchants#show'
 
