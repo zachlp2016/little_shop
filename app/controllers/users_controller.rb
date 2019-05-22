0a4c0b245f2595016cb6d0d97b816e7a251b6ad5
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-22 16:37:31 -0600

    Add logic from UsersController#create to #update, Add #update_email_confirmation(user_email) helper method to route Users correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,17 +33,32 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = current_user
-    user.update(strong_params)
-    flash[:notice] = "Your information has been updated!"
-
-    redirect_to profile_path
+    @user = current_user
+    if password_confirmation != true
+      flash.now[:notice] = "Those passwords don't match."
+      render :edit
+    elsif update_email_confirmation(@user.email) != true
+      flash.now[:notice] = "That email address is already taken."
+      render :edit
+    elsif @user.update!(strong_params)
+      flash[:notice] = "Your information has been updated!"
+      redirect_to profile_path
+    else
+      flash.now[:notice] = "That didn't work, please try again."
+      render :edit
+    end
   end
 
   private
 
   def email_confirmation
     User.email_string.all? do |email|
+      params[:user][:email] != email
+    end
+  end
+
+  def update_email_confirmation(user_email = nil)
+    (User.email_string - [user_email]).all? do |email|
       params[:user][:email] != email
     end
   end
