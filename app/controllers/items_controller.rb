cf418ae6fa0454e57721ea83e957731fd342616a
William Homer <billyhomeriii@gmail.com>
2019-05-22 19:53:49 -0600

    add instance method for item on show method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+
   def index
     @items = Item.all_active
     @most_5_bought_items = Item.most_5_bought
@@ -6,5 +7,6 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find_by(id: params['id'])
   end
 end
