6c8f76ffbab4b8a6919b27b4e98f0ab9fd032804
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-24 17:23:40 -0600

    Add OrderItems#sub_total spec to Model specs

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe OrderItem, type: :model do
     it { should validate_presence_of :quantity}
     it { should validate_presence_of :price}
   end
+
+  describe 'instance methods' do
+    before :each do
+      @user = User.create!(email: "test@test.com", password_digest: "t3s7", role: 1, active: true, name: "Testy McTesterson", address: "123 Test St", city: "Testville", state: "Test", zip: "01234")
+
+      @merchant_1 = create(:user)
+
+      @item_1 = create(:item, user: @merchant_1)
+
+      @order_1 = create(:order, user: @user)
+
+      @order_item_1 = create(:order_item, order: @order_1, item: @item_1, quantity: 2)
+    end
+
+    it '#sub_total' do
+      expect(@order_item_1.sub_total).to eq(19.98)
+    end
+  end
 end
