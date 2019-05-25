f808ecf73fd9afc838440a19415cd3b89c628b91
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-25 11:30:31 -0600

    Add #destroy to OrdersController that updates Order status to Cancelled, updates OrderItems fulfilled to False, flash notice confirmation, redirect to User Orders index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,17 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_items = @order.order_items
   end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.update(status: :cancelled)
+
+    @order.order_items.each do |order_item|
+      order_item.update(fulfilled: false)
+    end
+
+    flash[:notice] = "#{@order.id} has been cancelled."
+
+    redirect_to profile_orders_path
+  end
 end
