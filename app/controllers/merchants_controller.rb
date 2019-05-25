3b7331d390a107be1316b6d5dfd43024f900222f
William Homer <billyhomeriii@gmail.com>
2019-05-25 16:15:32 -0600

    add diable and enable methods to merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -8,4 +8,25 @@ class MerchantsController < ApplicationController
   def show
     @user = User.find(current_user.id)
   end
+
+  def disable
+    user = User.find_by(id: params[:id])
+    if User.update(user.id, active: false)
+      flash[:notice] = "Merchant's account is now disabled"
+    else
+      flash[:notice] = "Error on updating"
+    end
+    redirect_to merchants_path
+  end
+
+  def enable
+    user = User.find_by(id: params[:id])
+    if User.update(user.id, active: true)
+      flash[:notice] = "Merchant's account is now enabled"
+    else
+      flash[:notice] = "Error on updating"
+    end
+    redirect_to merchants_path
+  end
 end
+
