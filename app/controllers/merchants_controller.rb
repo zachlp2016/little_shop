836bd6f21f31581448469ad6ba8c7a095201a166
William Homer <billyhomeriii@gmail.com>
2019-05-25 14:50:32 -0600

    add logic for adim on merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,11 +1,11 @@
 class MerchantsController < ApplicationController
 
   def index
-    @merchants = User.all_active_merchants
+    @merchants = User.all_active_merchants if !current_admin?
+    @merchants = User.where(role: :merchant) if current_admin?
   end
 
   def show
     @user = User.find(current_user.id)
   end
-
 end
