e0ab70a3270f0ed6f62a87e719e26bad6d8cfa2d
Zach Leach <zachleach@techoverco.com>
2019-05-25 13:18:53 -0600

    Added test for to expect delete link for items that doesn't have buyers and verified its working.

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'As a merchant' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_1)
     end
 
-    it 'I see a link to add item to the system' do
+    it 'I see a list of the items that I have' do
       visit dashboard_items_path
 
       expect(page).to have_link('Add a new item')
@@ -50,10 +50,59 @@ RSpec.describe 'As a merchant' do
       end
 
       expect(page).to_not have_content(@item_4.name)
+      expect(page).to_not have_content(@item_5.name)
+    end
+  end
+
+  describe 'When I visit my items page, delete item only shows if a buyer isnt found' do
+
+    before :each do
+      @merchant_1 = create(:user, role: 1)
+      @merchant_2 = create(:user, role: 1)
+      @item_1 = create(:item, user: @merchant_1)
+      @item_2 = create(:item, user: @merchant_1)
+      @item_3 = create(:item, user: @merchant_1)
+      @item_4 = create(:item, user: @merchant_2)
+      @item_5 = create(:item, user: @merchant_2)
+      @user = create(:user)
+      order_1 = create(:order, user: @user)
+      OrderItem.create!(item: @item_2, order: order_1, quantity: 12, price: 1.99, fulfilled: true)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_1)
+    end
+
+    it 'has a link to delete the item if no user has ordered this item' do
+      # If no user has ever ordered this item, I see a link to delete the item
+      visit dashboard_items_path
+
+      expect(page).to have_link('Add a new item')
+
+      within "#item-#{@item_1.id}" do
+        expect(page).to have_content(@item_1.id)
+        expect(page).to have_content(@item_1.name)
+        expect(page).to have_content(@item_1.inventory)
+        expect(page).to have_content(@item_1.price)
+        expect(page).to have_link('Edit this item')
+        find "img[src='https://kaaskraam.com/wp-content/uploads/2018/02/Gouda-Belegen.jpg']"
+      end
+    end
+
+    it 'it doesnt have link to delete item if user is assigned' do
+      visit dashboard_items_path
+
+      expect(page).to have_link('Add a new item')
+
+      within "#item-#{@item_2.id}" do
+        expect(page).to have_content(@item_2.id)
+        expect(page).to have_content(@item_2.name)
+        expect(page).to have_content(@item_2.inventory)
+        expect(page).to have_content(@item_2.price)
+        expect(page).to have_link('Edit this item')
+        find "img[src='https://kaaskraam.com/wp-content/uploads/2018/02/Gouda-Belegen.jpg']"
+      end
     end
   end
 end
 
-# If no user has ever ordered this item, I see a link to delete the item
 # If the item is enabled, I see a button or link to disable the item
 # If the item is disabled, I see a button or link to enable the item
