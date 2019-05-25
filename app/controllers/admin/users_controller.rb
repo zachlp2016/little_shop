8b0e47c3ec8b99a528bc45c8e2597b51a489fede
William Homer <billyhomeriii@gmail.com>
2019-05-24 23:18:30 -0600

    add show method to admin users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,8 @@ class Admin::UsersController < ApplicationController
   def index
     @default_users = User.where(role: :default)
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 end
