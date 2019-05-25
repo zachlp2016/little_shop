dc2086c5640e5e21033c361e8d923aac68c16cf3
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-25 15:20:17 -0600

    Add after_save call to private method #update_inventory, using saved_change_to? hook to update associated Item inventory

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,7 +5,24 @@ class OrderItem < ApplicationRecord
   validates_presence_of :quantity, :price
   validates_inclusion_of :fulfilled, in: [true, false]
 
+  after_save :update_inventory
+
   def sub_total
     quantity * price
+  end
+
+  private
+
+  def update_inventory
+    if saved_change_to_fulfilled?
+
+      if fulfilled
+        new_inventory = item.inventory - quantity
+        item.update(inventory: new_inventory)
+      else
+        new_inventory = item.inventory + quantity
+        item.update(inventory: new_inventory)
+      end
+    end
   end
 end
