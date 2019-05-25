9377d35be5a49a164cb9a430fc771f147e2b8f6f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-25 15:18:53 -0600

    Add order_items loop setting fulfilled to false in OrdersController#destroy

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,11 +12,9 @@ class OrdersController < ApplicationController
   def destroy
     @order = Order.find(params[:id])
     @order.update(status: :cancelled)
-
     @order.order_items.each do |order_item|
       order_item.update(fulfilled: false)
     end
-
     flash[:notice] = "#{@order.id} has been cancelled."
 
     redirect_to profile_orders_path
