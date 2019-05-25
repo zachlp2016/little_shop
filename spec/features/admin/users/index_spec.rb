50b574fe069dae1ee3f50bd8ee690188f11c7b5b
William Homer <billyhomeriii@gmail.com>
2019-05-24 22:09:43 -0600

    add users link to navbar for admins only

---
+++ spec/features/admin/users/index_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'As an admin user' do
+  describe 'when i click a new Users link' do
+    before :each do
+      @admin_1 = User.create!(email: "ron_admin@gmail.com", password: "12345", role: 2, active: true, name: "Ron", address: "1234 Test Rd", city: "Kansas City", state: "MO", zip: '64086')
+
+      @merchant_1 = User.create!(email: "jon_mer@gmail.com", password: "12345", role: 1, active: true, name: "Jon a", address: "1234 Test Rd", city: "Kansas City", state: "MO", zip: '64086')
+      @merchant_2 = User.create!(email: "ron_mer@gmail.com", password: "12345", role: 1, active: true, name: "Ron b", address: "1234 Test Rd", city: "Kansas City", state: "MO", zip: '64086')
+      @user_1 = User.create!(email: "user1@gmail.com", password: "12345", role: 0, active: true, name: "Jon 1", address: "1234 Test Rd", city: "Kansas City", state: "MO", zip: '64086')
+      @user_2 = User.create!(email: "user2@gmail.com", password: "12345", role: 0, active: true, name: "Ron 2", address: "1234 Test Rd", city: "Kansas City", state: "MO", zip: '64086')
+      @user_3 = User.create!(email: "user3@gmail.com", password: "12345", role: 0, active: true, name: "Jon 3", address: "1234 Test Rd", city: "Kansas City", state: "MO", zip: '64086')
+    end
+
+    it 'shows all users who are not admins' do
+      visit root_path
+
+      click_on 'Users'
+      
+      expect(current_path).to eq('admin_users_path')
+
+      within "#user-#{@user_1.id}" do
+        expect(page).to have_content(@user_1.name) # ("/admin/users/5")
+        expect(page).to have_link(@user_1.name)
+        expect(page).to have_content(@user_1.created_at)
+        expect(page).to have_link('Upgrade to Merchant')
+      end
+      within "#user-#{@user_2.id}" do
+        expect(page).to have_content(@user_2.name) # ("/admin/users/5")
+        expect(page).to have_link(@user_2.name)
+        expect(page).to have_content(@user_2.created_at)
+        expect(page).to have_link('Upgrade to Merchant')
+      end
+      within "#user-#{@user_3.id}" do
+        expect(page).to have_content(@user_3.name) # ("/admin/users/5")
+        expect(page).to have_link(@user_3.name)
+        expect(page).to have_content(@user_3.created_at)
+        expect(page).to have_link('Upgrade to Merchant')
+      end
+
+      expect(page).to have_no_content(@merchant_1.name)
+      expect(page).to have_no_content(@merchant_2.name)
+      expect(page).to have_no_content(@admin_1.name)
+    end
+  end
+end
