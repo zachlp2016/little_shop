fb98c9f58337ae1b333bc1fa4124abe07d395ecd
Zach Leach <zachleach@techoverco.com>
2019-05-25 12:05:54 -0600

    Added test for the merchant index page to have a list of the merchants items."

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -7,15 +7,44 @@ RSpec.describe 'As a merchant' do
 
     before :each do
       @merchant_1 = create(:user)
+      @merchant_2 = create(:user)
       @item_1 = create(:item, user: @merchant_1)
       @item_2 = create(:item, user: @merchant_1)
       @item_3 = create(:item, user: @merchant_1)
+      @item_4 = create(:item, user: @merchant_2)
+      @item_5 = create(:item, user: @merchant_2)
     end
 
     it 'I see a link to add item to the system' do
       visit dashboard_items_path
 
       expect(page).to have_link('Add a new item')
+
+      within "#item-#{@item_1.id}" do
+        expect(page).to have_content(@item_1.name)
+        expect(page).to have_content(@item_1.user.name)
+        expect(page).to have_content(@item_1.inventory)
+        expect(page).to have_content(@item_1.price)
+        find "img[src='https://kaaskraam.com/wp-content/uploads/2018/02/Gouda-Belegen.jpg']"
+      end
+
+      within "#item-#{@item_2.id}" do
+        expect(page).to have_content(@item_2.name)
+        expect(page).to have_content(@item_2.user.name)
+        expect(page).to have_content(@item_2.inventory)
+        expect(page).to have_content(@item_2.price)
+        find "img[src='https://kaaskraam.com/wp-content/uploads/2018/02/Gouda-Belegen.jpg']"
+      end
+
+      within "#item-#{@item_3.id}" do
+        expect(page).to have_content(@item_3.name)
+        expect(page).to have_content(@item_3.user.name)
+        expect(page).to have_content(@item_3.inventory)
+        expect(page).to have_content(@item_3.price)
+        find "img[src='https://kaaskraam.com/wp-content/uploads/2018/02/Gouda-Belegen.jpg']"
+      end
+
+      expect(page).to_not have_content(@item_4.name)
     end
   end
 end
