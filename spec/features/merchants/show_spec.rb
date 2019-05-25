647773cd831e32871f574d8d8be0c34c5911cdda
Zach Leach <zachleach@techoverco.com>
2019-05-25 17:45:30 -0600

    Added final test for orders to show on the merchant dashboard and verified it's working.

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe 'Merchant show page', type: :feature do
         @order_3 = create(:order, user: @user_3, status: 0)
         @order_4 = create(:order, user: @user_3, status: 0)
         OrderItem.create!(item: @item_1, order: @order_1, quantity: 12, price: 1.99, fulfilled: true)
-        OrderItem.create!(item: @item_2, order: @order_2, quantity: 12, price: 1.99, fulfilled: true)
-        OrderItem.create!(item: @item_3, order: @order_3, quantity: 12, price: 1.99, fulfilled: true)
-        OrderItem.create!(item: @item_3, order: @order_4, quantity: 12, price: 1.99, fulfilled: true)
+        OrderItem.create!(item: @item_2, order: @order_2, quantity: 13, price: 1.99, fulfilled: true)
+        OrderItem.create!(item: @item_3, order: @order_3, quantity: 14, price: 1.99, fulfilled: true)
+        OrderItem.create!(item: @item_3, order: @order_4, quantity: 15, price: 1.99, fulfilled: true)
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
       end
@@ -43,6 +43,7 @@ RSpec.describe 'Merchant show page', type: :feature do
       it 'displays the orders for items a merchent sells' do
         visit dashboard_path
 
+
         within("#order-#{@order_1.id}") do
           expect(page).to have_link("#{@order_1.id}")
           expect(page).to have_content("Date created: #{@order_1.date_made}")
@@ -52,24 +53,13 @@ RSpec.describe 'Merchant show page', type: :feature do
 
         within("#order-#{@order_2.id}") do
           expect(page).to have_link("#{@order_2.id}")
-          expect(page).to have_content("Date created: #{@order_1.date_made}")
-          expect(page).to have_content("Total quantity: #{@order_1.item_count}")
-          expect(page).to have_content("Total price: #{@order_1.grand_total}")
+          expect(page).to have_content("Date created: #{@order_2.date_made}")
+          expect(page).to have_content("Total quantity: #{@order_2.item_count}")
+          expect(page).to have_content("Total price: #{@order_2.grand_total}")
         end
 
-        expect(page).to_not have_link("#{@order_1.id}")
-        expect(page).to_not have_content("Date created: #{@order_3.date_made}")
-        expect(page).to_not have_content("Total quantity: #{@order_3.item_count}")
-        expect(page).to_not have_content("Total price: #{@order_3.grand_total}")
-        expect(page).to_not have_content("Order id: #{@order_4.id}")
-
+        expect(page).to_not have_link("#{@order_3.id}")
       end
     end
   end
 end
-
-
-# - the ID of the order, which is a link to the order show page ("/dashboard/orders/15")
-# - the date the order was made
-# - the total quantity of my items in the order
-# - the total value of my items for that order
