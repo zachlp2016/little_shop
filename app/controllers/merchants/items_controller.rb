2966b04929e074477bd0cbbc64b9869e84b1d697
Zach Leach <zachleach@techoverco.com>
2019-05-25 19:43:02 -0600

    Added item destroy method to the items controller.

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -3,4 +3,9 @@ class Merchants::ItemsController < ApplicationController
   def index
     @merchant = User.find(current_user.id)
   end
+
+  def destroy
+    @item = Item.destroy(params[:id])
+    redirect_to dashboard_items_path
+  end
 end
