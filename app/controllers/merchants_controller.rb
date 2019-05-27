dea49b8a35f794c8114bf537a1a00f80255f8630
Vince Carollo <vincecarollo@gmail.com>
2019-05-26 22:18:19 -0600

    Start users controller interaction with merchant index page for stats

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,6 +2,9 @@ class MerchantsController < ApplicationController
 
   def index
     @merchants = User.all_active_merchants
+    @top_merchants = User.top_3_merchants_by_sales
+    @fastest_merchants = User.fastest_3_fulfilling_merchants
+    @slowest_merchants = User.slowest_3_fulfilling_merchants
   end
 
   def show
