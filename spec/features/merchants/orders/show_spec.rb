0ee43c58a071dbe9ced94d5e85febfe4e5728769
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-27 16:04:09 -0600

    Add Displays Only My Orders spec to Merchant Order Show spec

--- spec/features/merchants/orders/show_spec.rb
+++ spec/features/merchants/orders/show_spec.rb
@@ -27,5 +27,25 @@ RSpec.describe 'As a merchant', type: :feature do
       expect(page).to have_content("Name: Testy McTesterson")
       expect(page).to have_content("Address: 123 Test St, Testville, Test, 01234")
     end
+
+    it 'Displays only my Items in the Order' do
+      visit dashboard_order_path(@order)
+
+      within("#item-#{@item_1.id}") do
+        expect(page).to have_button(@item_1.name, href: item_path(@item_1))
+        find "img[src='https://kaaskraam.com/wp-content/uploads/2018/02/Gouda-Belegen.jpg']"
+        expect(page).to have_content(@item_1.price)
+        expect(page).to have_content(@order_item_1.quantity)
+      end
+
+      within("#item-#{@item_2.id}") do
+        expect(page).to have_button(@item_2.name, href: item_path(@item_1))
+        find "img[src='https://kaaskraam.com/wp-content/uploads/2018/02/Gouda-Belegen.jpg']"
+        expect(page).to have_content(@item_2.price)
+        expect(page).to have_content(@order_item_2.quantity)
+      end
+
+      expect(page).to_not have_css("#item-#{@item_3.id}")
+    end
   end
 end
