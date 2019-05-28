c8b9e0cbe61faa3d2df645f484b674b2e95c5b7c
Vince Carollo <vincecarollo@gmail.com>
2019-05-28 13:19:41 -0600

    Change merchants/items_controller create action to not have to check for price or inventory, and to display all errors if form is filled out incorrectly

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -1,5 +1,4 @@
 class Merchants::ItemsController < ApplicationController
-
   def index
     @merchant = User.find(current_user.id)
   end
@@ -10,22 +9,14 @@ class Merchants::ItemsController < ApplicationController
   end
 
   def create
-    if verify_price(params) == false
-      flash[:notice] = "The price for that item cannot be negative."
-      redirect_to dashboard_items_path
-    elsif verify_inventory_count(params) == false
-      flash[:notice] = "The inventory for that item cannot be a negative number."
+    verify_image(params)
+    @merchant = User.find(current_user.id)
+    @item = @merchant.items.new(items_params)
+    if @item.save
+      flash[:notice] = "The item was created successfully."
       redirect_to dashboard_items_path
     else
-      verify_image(params)
-      @merchant = User.find(current_user.id)
-      @item = @merchant.items.create!(items_params)
-      if @item.save
-        flash[:notice] = "The item was created successfully."
-        redirect_to dashboard_items_path
-      else
-        render :new
-      end
+      render :new
     end
   end
 
@@ -36,7 +27,7 @@ class Merchants::ItemsController < ApplicationController
     flash[:notice] = "This item has been disabled."
     redirect_to dashboard_items_path
   end
-    
+
   def enable
     @item = Item.find(params[:id])
     @item.active = true
@@ -55,18 +46,6 @@ class Merchants::ItemsController < ApplicationController
 
   def items_params
     params.require(:item).permit(:name, :price, :description, :image, :inventory )
-  end
-
-  def verify_price(params)
-    if params["item"]["price"].to_i < 0
-      return false
-    end
-  end
-
-  def verify_inventory_count(params)
-    if params["item"]["inventory"].to_i < 0
-      return false
-    end
   end
 
   def verify_image(params)
