ec1845930ee8a73e082d03efe15018ecf8eb34cf
Vince Carollo <vincecarollo@gmail.com>
2019-05-29 22:43:22 -0600

    Change new item form to allow cents

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -23,10 +23,11 @@ class Cart
   end
 
   def create_order(buyer_id)
-    order = Order.create(user_id: buyer_id, status: 1)
+    order = Order.new(user_id: buyer_id, status: 1)
     ids_to_items.each do |item, qty|
       oi = OrderItem.new(item: item, order: order, quantity: qty, price: item.price, fulfilled: false )
       oi.save
     end
+    order.save
   end
 end
