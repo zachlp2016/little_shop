e92ace932429090e5b2f5770cd5bc9774f91b47e
Zach Leach <zachleach@techoverco.com>
2019-06-01 13:51:05 -0600

    Added test that the new address can be created.

--- app/controllers/default/addresses_controller.rb
+++ app/controllers/default/addresses_controller.rb
@@ -1,7 +1,24 @@
 class Default::AddressesController < Default::BaseController
 
   def new
-
+    @user = current_user
+    @address = @user.addresses.new
   end
 
+  def create
+    @user = current_user
+    @address = @user.addresses.create!(address_params)
+    if @address.save
+      flash[:notice] = "You have added a new address"
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:nickname, :street, :city, :state, :zip)
+  end
 end
--- spec/features/default/addresses/new_spec.rb
+++ spec/features/default/addresses/new_spec.rb
@@ -1,12 +1,42 @@
-# it 'Allows User to add a new address' do
-#   visit profile_path
-#
-#   within ".add-address" do
-#     click_link('Add Address')
-#   end
-#
-#   expect(current_path).to eq(new_address_path)
-#
-#
-#
-# end
+require 'rails_helper'
+
+RSpec.describe 'User new address page', type: :feature do
+  context 'As a regular user' do
+    describe 'When I add a new address' do
+      before :each do
+        @user = User.create!(email: "test@test.com", password_digest: "t3s7", role: 0, active: true, name: "Testy McTesterson", address: "123 Test St", city: "Testville", state: "Test", zip: "01234")
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      end
+
+      it 'Im taken to a new address form' do
+        visit new_address_path
+
+        expect(page).to have_content('Create a New Address')
+
+        fill_in 'Nickname', with: 'New Address'
+        fill_in 'Street', with: '1111 New Street'
+        fill_in 'City', with: 'Denver'
+        fill_in 'State', with: 'CO'
+        fill_in 'Zip', with: '80226'
+
+        click_button('Create Address')
+
+        last_address = @user.addresses.last
+
+        expect(page).to have_content('You have added a new address')
+
+        within ".other-address-#{last_address.id}" do
+          expect(page).to have_content("Nickname: #{last_address.nickname}")
+          expect(page).to have_content("Nickname: #{last_address.street}")
+          expect(page).to have_content("Nickname: #{last_address.city}")
+          expect(page).to have_content("Nickname: #{last_address.state}")
+          expect(page).to have_content("Nickname: #{last_address.zip}")
+        end
+
+        expect(page).to have_content()
+        expect(last_address.nickname).to eq('New Address')
+      end
+    end
+  end
+end
